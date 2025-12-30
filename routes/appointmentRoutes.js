58cc22f1b3cb4ec56a172cc2d5bbe2a5a7d88fe0
Nivi Jha <nivijha2004@gmail.com>
2025-12-30 23:27:55 +0530

    Fix appointments API routes

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -3,7 +3,6 @@ import protect from "../middleware/authMiddleware.js";
 import {
   createAppointment,
   getAppointments,
-  updateAppointment,
   cancelAppointment,
   deleteAppointment
 } from "../controllers/appointmentController.js";
@@ -14,7 +13,6 @@ router.use(protect);
 
 router.post("/", createAppointment);
 router.get("/", getAppointments);
-router.put("/:id", updateAppointment);
 router.put("/:id/cancel", cancelAppointment);
 router.delete("/:id", deleteAppointment);
 
